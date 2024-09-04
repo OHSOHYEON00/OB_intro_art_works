21f8f04b91e515dd9549cdb38abe779fea60c143
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-09-04 13:26:38 +0200

    Init Setup with CRA

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, {  useState } from "react";
+import "./App.css";
+import mainBg from "./mainBg.png"
+import Loading from "./Loading.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Card from "./Card.jsx";
+
+function App() {
+
+
+  const Main = () => {
+    const [show, setIsShow] = useState(false);
+  
+      return (
+        <div className={`cotainer flex justify-center items-center h-scree`}>
+        <div className="max-h-[900px] flex justify-center w-screen h-screen items-center max-w-[480px] shadow-[0px_0px_12px_10px_rgb(144_144_255_/_20%)] ">
+          <img alt="bg" src={mainBg} width={300} height={500} />
+          <div className="absolute font-['DungGeunMo'] text-xl right-[30%] top-[50%] hover:cursor-pointer animate-bounce" onClick={() => setIsShow(true)}>
+            <div className=" bg-[#d0d0d0] p-[8px]">동전넣기</div>
+            <div className="absolute left-1/2 transform -translate-x-1/2 top-full">
+              <div className="w-0 h-0 border-l-[10px] border-r-[10px] border-t-[18px] border-l-transparent border-r-transparent border-t-[#d0d0d0]"></div>
+            </div>
+          </div>
+        </div>
+  
+        {show && <Loading />}
+        </div>
+      )
+    };
+
+  return (
+    <Router>
+    <Routes>
+      <Route path='/' element={ <Main />} />
+      <Route path='/card' element={ <Card />} />
+    </Routes>
+    </Router>
+  );
+}
+
+export default App;
