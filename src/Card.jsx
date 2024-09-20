53cfb3875ccbe2a1f9b6682cb4d7e30c4f0d8485
OHSOHYEON00 <ohsohyeon24@gmail.com>
2024-09-20 11:54:41 +0200

    Improve UX on Card page refresh by redirecting to the home page
    - Add new card, icon images
    - Improve UX on loading image

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import card2 from "./img/2.png";
-import card1 from "./img/1.png";
-import icon from "./icon.png";
+import restartIcon from "./restart.png";
+import shareIcon from "./share.png";
 
 const Card = () => {
   const navigate = useNavigate();
   const [randomImg, setRandomImg] = useState();
+  const [loading, setLoading] = useState(true);
 
   const handleCopyClipBoard = async (text) => {
     try {
@@ -17,10 +17,12 @@ const Card = () => {
     }
   };
 
-  const imageList = [card1, card2];
+  const imageList = Array.from({ length: 30 }).map(
+    (v, i) => `Artboard ${i + 1}CARD.png`
+  );
 
   const showImage = useCallback(() => {
-    const imgNum = Math.round(Math.random() * (imageList.length - 1));
+    const imgNum = Math.round(Math.random() * imageList.length);
     setRandomImg(imgNum);
   }, []);
 
@@ -28,7 +30,7 @@ const Card = () => {
     window.onload = showImage();
   }, [showImage]);
 
-  const Button = ({ text, onClick }) => {
+  const Button = ({ text, onClick, icon }) => {
     return (
       <div
         className="flex flex-col items-center hover:cursor-pointer"
@@ -49,6 +51,13 @@ const Card = () => {
     window.addEventListener("resize", sv);
     window.addEventListener("load", sv);
 
+    if (window.performance) {
+      const [navigation] = window.performance.getEntriesByType("navigation");
+      if (navigation.type === "reload") {
+        window.location.replace(`${process.env.PUBLIC_URL}/`); // A 페이지로 강제 이동
+      }
+    }
+
     return () => {
       window.removeEventListener("resize", sv);
       window.removeEventListener("load", sv);
@@ -61,13 +70,18 @@ const Card = () => {
         "cotainer custom-card h-screen-dynamic flex items-center flex-col"
       }
     >
-      {/* md:h-[60%] md:w-auto */}
+      {loading && (
+        <div className="shadow-3xl w-[320px] h-auto md:h-[60%] md:w-auto mt-[10vh] mx-10"></div>
+      )}
       <img
-        className="shadow-3xl w-[60%] h-auto md:h-[60%] md:w-auto mt-[10vh] mx-10"
-        src={imageList[randomImg]}
+        className={`shadow-3xl w-[60%] h-auto md:h-[60%] md:w-auto mt-[10vh] mx-10 ${
+          loading ? "hidden" : "block"
+        }`}
+        src={`/card/Artboard ${randomImg}CARD.png`}
         alt="card"
         width={320}
         height={500}
+        onLoad={() => setLoading(false)}
       />
       <div className="font-['DungGeunMo'] text-[1rem] sm:text-[1.5rem] md:text-[1.7rem] mt-7 animate-bounce">
         <span className="mr-1">&#9650;</span>꾹 눌러서 수집
@@ -76,8 +90,13 @@ const Card = () => {
         <Button
           text={"다시하기"}
           onClick={() => navigate(`${process.env.PUBLIC_URL}/`)}
+          icon={restartIcon}
         />
-        <Button text={"공유하기"} onClick={handleCopyClipBoard} />
+        <Button
+          text={"공유하기"}
+          onClick={handleCopyClipBoard}
+          icon={shareIcon}
+        />
       </div>
     </div>
   );
